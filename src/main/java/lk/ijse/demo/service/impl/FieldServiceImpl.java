c08114033229331d1f80b6f7b029af9499992ad5
Saami <uthpala2001@gmail.com>
2024-11-28 11:38:40 +0530

    updateField done

--- src/main/java/lk/ijse/demo/service/impl/FieldServiceImpl.java
+++ src/main/java/lk/ijse/demo/service/impl/FieldServiceImpl.java
@@ -4,18 +4,23 @@ import jakarta.transaction.Transactional;
 import lk.ijse.demo.dao.CropDAO;
 import lk.ijse.demo.dao.FieldDAO;
 import lk.ijse.demo.dao.StaffDAO;
+import lk.ijse.demo.dto.FieldStatus;
 import lk.ijse.demo.dto.impl.FieldDTO;
 import lk.ijse.demo.entity.impl.CropEntity;
 import lk.ijse.demo.entity.impl.FieldEntity;
 import lk.ijse.demo.entity.impl.StaffEntity;
 import lk.ijse.demo.exception.DataPersistException;
+import lk.ijse.demo.exception.FieldNotFoundException;
 import lk.ijse.demo.service.FieldService;
 import lk.ijse.demo.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -32,36 +37,6 @@ public class FieldServiceImpl implements FieldService {
 
     @Override
     public void saveField(FieldDTO fieldDTO) {
-//        int number = 0;
-//        FieldEntity field = fieldDAO.findLastRowNative();
-//        if (field != null) {
-//            String[] parts = field.getFieldCode().split("-");
-//            number = Integer.parseInt(parts[1]);
-//        }
-//        fieldDTO.setFieldCode("FIELD-" + ++number);
-//        List<StaffEntity> staffEntities = new ArrayList<>();
-//        List<CropEntity> cropEntities = new ArrayList<>();
-//        if (fieldDTO.getMemberCodeList() != null || fieldDTO.getCropCodeList() != null) {
-//            for (String id : fieldDTO.getMemberCodeList()) {
-//                if (staffDAO.existsById(id)) {
-//                    staffEntities.add(staffDAO.getReferenceById(id));
-//                }
-//            }
-//            for (String id : fieldDTO.getCropCodeList()) {
-//                if (cropDAO.existsById(id)) {
-//                    cropEntities.add(cropDAO.getReferenceById(id));
-//                }
-//            }
-//        }
-//        FieldEntity fieldEntity = mapping.toFieldEntity(fieldDTO);
-//        fieldEntity.setLocation(fieldDTO.getLocation());
-//        fieldEntity.setStaffList(staffEntities);
-//        fieldEntity.setCropList(cropEntities);
-//        FieldEntity saveField = fieldDAO.save(fieldEntity);
-//        if (saveField == null) {
-//            throw new DataPersistException("Field is not saved.");
-//        }
-//    }
         int number = 0;
         FieldEntity field = fieldDAO.findLastRowNative();
         if (field != null) {
@@ -100,7 +75,45 @@ public class FieldServiceImpl implements FieldService {
         // Save the entity and handle the result
         FieldEntity savedField = fieldDAO.save(fieldEntity);
         if (savedField == null) {
-            throw new DataPersistException("Field is not saved.");
+            throw new DataPersistException("Field Is Not Saved.");
         }
+    }
+
+
+    @Override
+    public List<FieldDTO> getAllField() throws IOException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteField(String id) throws FileNotFoundException, FieldNotFoundException {
+
+    }
+
+    @Override
+    public void updateField(String id, FieldDTO fieldDTO) {
+        Optional<FieldEntity> byId = fieldDAO.findById(id);
+        if (byId.isPresent()){
+            byId.get().setName(fieldDTO.getName());
+            byId.get().setLocation(fieldDTO.getLocation());
+            byId.get().setExtentSize(fieldDTO.getExtentSize());
+            byId.get().setFieldImage1(fieldDTO.getFieldImage1());
+            byId.get().setFieldImage2(fieldDTO.getFieldImage2());
+            List<CropEntity> cropEntities = new ArrayList<>();
+            List<StaffEntity> staffEntities = new ArrayList<>();
+            for (String cropCode:fieldDTO.getCropCodeList()){
+                cropEntities.add(cropDAO.getReferenceById(cropCode));
+            }
+            for (String memberCode:fieldDTO.getMemberCodeList()){
+                staffEntities.add(staffDAO.getReferenceById(memberCode));
+            }
+            byId.get().setCropList(cropEntities);
+            byId.get().setStaffList(staffEntities);
+        }
+    }
+
+    @Override
+    public FieldStatus getSelectedField(String fieldId) {
+        return null;
     }
 }
