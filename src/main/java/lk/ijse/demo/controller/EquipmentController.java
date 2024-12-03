a739ef091bacb9daceacd9c2c75e4085e754d8e2
Saami <uthpala2001@gmail.com>
2024-12-03 09:23:10 +0530

    getAllEquipment done

--- src/main/java/lk/ijse/demo/controller/EquipmentController.java
+++ src/main/java/lk/ijse/demo/controller/EquipmentController.java
@@ -14,6 +14,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/equipment")
 @CrossOrigin
@@ -22,19 +24,18 @@ public class EquipmentController {
     private EquipmentService equipmentService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO equipmentDTO) {
-        try {
+    public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO equipmentDTO){
+        try{
             equipmentService.saveEquipment(equipmentDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (DataPersistException e) {
+        }catch (DataPersistException e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
     @PutMapping(value = "/{equipmentId}",consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> updateEquipment(@PathVariable ("equipmentId") String equipmentId ,@RequestBody EquipmentDTO equipmentDTO){
         try{
@@ -69,4 +70,10 @@ public class EquipmentController {
         }
         return equipmentService.getSelectedEquipment(equipmentId);
     }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EquipmentDTO> getAllEquipment(){
+        return equipmentService.getAllEquipment();
+    }
+
 }
