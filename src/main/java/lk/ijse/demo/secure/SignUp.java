e49ca71015fedc885088c0cc1527aa84cfcb7e54
Saami <uthpala2001@gmail.com>
2024-12-05 22:41:01 +0530

    bind annotation done

--- src/main/java/lk/ijse/demo/secure/SignUp.java
+++ src/main/java/lk/ijse/demo/secure/SignUp.java
@@ -6,21 +6,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@CrossOrigin
+
 public class SignUp {
     @NotNull
     private String user_id;
     @NotNull(message = "Email Can`t be Null ")
     @Email
     private String email;
-
     @NotNull
     private String password;
-
     @NotNull
     private String role;
 
--- src/main/java/lk/ijse/demo/secure/SingIn.java
+++ src/main/java/lk/ijse/demo/secure/SingIn.java
@@ -6,10 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@CrossOrigin
+
+
 public class SingIn {
     @NotNull(message = "Email Can`t be Null")
     @Email
