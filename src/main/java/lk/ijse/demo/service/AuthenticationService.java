35ceefaf2ee81223b0239bd140b15f06402efeab
Saami <uthpala2001@gmail.com>
2024-12-05 22:42:13 +0530

    AuthenticationService done

---
+++ src/main/java/lk/ijse/demo/service/AuthenticationService.java
@@ -0,0 +1,13 @@
+package lk.ijse.demo.service;
+
+import lk.ijse.demo.response.JwtAuthResponse;
+import lk.ijse.demo.secure.SignUp;
+import lk.ijse.demo.secure.SingIn;
+
+public interface AuthenticationService {
+    JwtAuthResponse signUp (SignUp signUp);
+    JwtAuthResponse signIn (SingIn signUp);
+    JwtAuthResponse refreshToken(String refreshToken);
+//    void changePassword(ChangePasswordDTO changePasswordDTO);
+
+}
