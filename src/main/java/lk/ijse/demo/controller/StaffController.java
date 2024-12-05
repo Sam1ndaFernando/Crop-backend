a1d19c507292d65c0b3c06a7477847a0bff340fb
Saami <uthpala2001@gmail.com>
2024-12-05 22:37:25 +0530

    implement security features in StaffController

--- src/main/java/lk/ijse/demo/controller/StaffController.java
+++ src/main/java/lk/ijse/demo/controller/StaffController.java
@@ -11,75 +11,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/staff")
-@CrossOrigin( origins = "http://localhost:63343")
+@CrossOrigin
 public class StaffController {
     @Autowired
     private StaffService staffService;
-
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO) {
-        try {
-            if (!Regex.emailValidator(staffDTO.getEmail()).matches()) {
+    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO){
+        try{
+            if (!Regex.emailValidator(staffDTO.getEmail()).matches()){
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             staffService.saveStaffMember(staffDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (DataPersistException e) {
+        }catch (DataPersistException e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-    @PutMapping(value = "/{staffId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> updateStaff(@PathVariable("staffId") String staffId, @RequestBody StaffDTO staffDTO) {
-        try {
-            staffService.updateStaffMember(staffId, staffDTO);
+    // melkata damnna one SCIENTIST mewa danna get all witharai
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
+    @PutMapping(value = "/{staffId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateStaff(@PathVariable ("staffId") String staffId ,@RequestBody StaffDTO staffDTO){
+        try{
+            staffService.updateStaffMember(staffId,staffDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (DataPersistException e) {
+        }catch (DataPersistException e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
     @DeleteMapping(value = "/{staffId}")
-    public ResponseEntity<Void> deleteStaff(@PathVariable("staffId") String staffId) {
-        try {
-            if (!Regex.idValidator(staffId).matches()) {
+    public ResponseEntity<Void> deleteStaff(@PathVariable ("staffId") String staffId){
+        try{
+            if (!Regex.idValidator(staffId).matches()){
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             staffService.deleteStaffMember(staffId);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (StaffNotFoundException e) {
+        }catch (StaffNotFoundException e){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<StaffDTO> getAllStaff() {
+    public List<StaffDTO> getAllStaff(){
 
         return staffService.getAllStaffMember();
     }
-
-    @GetMapping(value = "/{staffId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public StaffStatus getSelectedStaff(@PathVariable("staffId") String staffId) {
-        if (!Regex.idValidator(staffId).matches()) {
-            return new SelectedErrorStatus(1, "Staff Code Not Valid");
+    @GetMapping(value = "/{staffId}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public StaffStatus getSelectedStaff(@PathVariable("staffId") String staffId){
+        if (!Regex.idValidator(staffId).matches()){
+            return new SelectedErrorStatus(1,"Staff Code Not Valid");
         }
         return staffService.getSelectedStaffMember(staffId);
     }
 }
-
