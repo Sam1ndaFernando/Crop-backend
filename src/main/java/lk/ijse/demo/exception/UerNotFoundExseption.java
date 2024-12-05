cabec371e3e393a4334f24776ccef3f6031e3279
Saami <uthpala2001@gmail.com>
2024-12-05 22:40:27 +0530

    bug fix

--- src/main/java/lk/ijse/demo/exception/UerNotFoundExseption.java
+++ src/main/java/lk/ijse/demo/exception/UerNotFoundExseption.java
@@ -7,7 +7,7 @@ public class UerNotFoundExseption extends RuntimeException {
     public UerNotFoundExseption(String message){
         super(message);
     }
-    public UerNotFoundExseption(String message, Throwable cause){
+    public UerNotFoundExseption(String message,Throwable cause){
         super(message,cause);
     }
 }
