ce83dc50a51630f21fa645a5bb303bd39d31570f
Saami <uthpala2001@gmail.com>
2024-12-05 22:37:59 +0530

    implement security features in VehicleController

--- src/main/java/lk/ijse/demo/controller/VehicleController.java
+++ src/main/java/lk/ijse/demo/controller/VehicleController.java
@@ -11,71 +11,79 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/vehicles")
-@CrossOrigin( origins = "http://localhost:63343")
+@CrossOrigin
 public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
 
+
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO vehicleDTO) {
+    public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO vehicleDTO){
         System.out.println(vehicleDTO);
-        try {
+        try{
             vehicleService.saveVehicle(vehicleDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (DataPersistException e) {
+        }catch (DataPersistException e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<VehicleDTO> getAllVehicles() {
+    public List<VehicleDTO> getAllVehicles(){
         return vehicleService.getAllVehicle();
     }
 
-    @PutMapping(value = "/{vehicleId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> updateVehicle(@PathVariable("vehicleId") String vehicleId, @RequestBody VehicleDTO vehicleDTO) {
-        try {
-            vehicleService.updateVehicle(vehicleId, vehicleDTO);
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
+    @PutMapping(value = "/{vehicleId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateVehicle(@PathVariable ("vehicleId") String vehicleId ,@RequestBody VehicleDTO vehicleDTO){
+        try{
+            vehicleService.updateVehicle(vehicleId,vehicleDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (DataPersistException e) {
+        }catch (DataPersistException e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
     @DeleteMapping(value = "/{vehicleId}")
-    public ResponseEntity<Void> deleteVehicle(@PathVariable("vehicleId") String vehicleId) {
-        try {
-            if (!Regex.idValidator(vehicleId).matches()) {
+    public ResponseEntity<Void> deleteVehicle(@PathVariable ("vehicleId") String vehicleId){
+        try{
+            if (!Regex.idValidator(vehicleId).matches()){
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             vehicleService.deleteVehicle(vehicleId);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (VehicleNotFoundException e) {
+        }catch (VehicleNotFoundException e){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-    @GetMapping(value = "/{vehicleId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public VehicleStatus getSelectedVehicle(@PathVariable("vehicleId") String vehicleId) {
-        if (!Regex.idValidator(vehicleId).matches()) {
-            return new SelectedErrorStatus(1, "Vehicle Code Not Valid");
+    @PreAuthorize("hasAnyRole('MANAGER','ADMINISTRATIVE')")
+    @GetMapping(value = "/{vehicleId}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public VehicleStatus getSelectedVehicle(@PathVariable("vehicleId") String vehicleId){
+        if (!Regex.idValidator(vehicleId).matches()){
+            return new SelectedErrorStatus(1,"Vehicle Code Not Valid");
         }
         return vehicleService.getSelectedVehicle(vehicleId);
     }
+
+
+
 
 }
